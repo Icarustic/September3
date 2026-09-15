cfdb443a14083c652bf27ff59f6f5e470a63aabc
A00644799 <A00644799@76QHF24>
2026-09-15 11:32:25 -0500

    Added September 15

--- September11/September11.cpp
+++ September11/September11.cpp
@@ -3,65 +3,72 @@
 #include<string>
 #include<algorithm>
 #include<iomanip>
-//#include <windows.h>
-#include <io.h> // This is a library that allows you to use functions like _setmode() and _fileno() to change the output mode of the console.
-#include <fcntl.h> // This is a library that allows you to use functions like _O_U16TEXT to set the output mode of the console to Unicode.
 
-using namespace std; 
+using namespace std;
+// We are going to write to an Output file here. Saving our results to a file.
+#include<fstream> // f stands for file.
+#include<iostream> // io stands for input/output.
+
+using namespace std;
 
 int main()
 {
-    //---Input and Output Practice---
+    ofstream fout("SomeText.txt"); //output file fout(filename) //the .txt is a file extension.
 
-    //---UsingPowFunction---
-    //int angstromsPerMeter = pow(10, 10);
-    //cout << angstromsPerMeter;
-    int micrometer;
-    micrometer = pow(10, 6);
-    cout << "Micrometer: " << micrometer << "\n";
-    long long int angstromsPerMeter; // replace int -> long long int 
-    angstromsPerMeter = pow(10, 10);
-    cout << "Angstrom: " << angstromsPerMeter << "\n"; // fails due to overfloow instead use long long data type instead
-
-    cout << "\n"; //Spacer
-    //---String Length Example---
-    string Name_1 = "Evan Cokrell";
-    string Name_2 = "Roderick Walker";
-    int result = Name_1.length() - count(Name_1.begin(), Name_1.end(), ' '); // this correctly removes spaces from the count
-    cout << "Name 1 Lenght: " << result << "\n";
-    int result2 = Name_2.length() - count(Name_2.begin(), Name_2.end(), ' '); // this correctly removes spaces from the count
-    cout << "Name 2 Lenght: " << result2 << "\n";
-
-    //---TotalCostProgram---
-    cout << fixed << setprecision(2); // This is how to use setprecision, which allows you to set the degree of precision for decimals and whole numbers. With 2 meaning 00.00 anything larger or smaller is rounded to the nearest 2 decimal places.
-    // Specifically set fixed so it keeps all values to the degree of values you set, basicly makes sure the result has decimal points.
-    float TotalCost;
-    float Product_1_Price; //Use float instead of int so it can store the decimels representing cents
-    float Product_2_Price;
-    float Product_3_Price;
-    //getline()
-    //while (true) // For testing the code repeatedly
+    //Loop memory error
+    //while (true)
     //{
-    cout << "\nWhat is the price of Product 1?\n";
-    //cin >> Product_1_Price;
-    Product_1_Price = 19.16; // Sani Cloth 
-    cout << "Product 1 Price: $" << Product_1_Price << "\n";
-    cout << "What is the price of Product 2?\n";
-    //cin >> Product_2_Price;
-    Product_2_Price = 98.24; // Clean Good 
-    cout << "Product 2 Price: $" << Product_2_Price << "\n";
-    cout << "What is the price of Product 3?\n";
-    Product_3_Price = 349.99; // Revolution Hydro Clean
-    cout << "Product 3 Price: $" << Product_3_Price << "\n";
-    //cin >> Product_3_Price;
-
-    //if (cin.fail())
-    //{
-    //	cin.clear(); 
+    //    fout << "a\n";
     //}
 
-    TotalCost = Product_1_Price + Product_2_Price + Product_3_Price;
-    cout << "TotalCost: $" << TotalCost; //Excluding tax, this is the total cost of the products.
+    //fout << "asdasdf\n";
+    //fout << "This is the second line of output written to the file\n";
 
-    //}
+    fout.close();
+    //The output is found through open in file explorer and open the text file in the project.
+    //close the file when done writing to it. This is important because if you don't close the file, the data may not be saved properly.
+        // file extensions are used by an OS to determine the app used to open the file
+        //ex: .jpg files open with windows photo viewer, .mp3 files open with windows media player, .txt files open with notepad, etc.
+        // .txt files open with notepad
+
+    //read input from a already existing file:
+    ifstream fin("SomeText.txt");
+
+    if (fin.is_open() == false) //if the file was not found or opened
+    {
+        cout << "file was not found\n";
+        return -1; // early return
+    }
+
+    string firstLineOfFile;
+    getline(fin, firstLineOfFile); //Similar to how we used it before but translated from getline(cin, variable);
+
+    cout << "Does this line get printed if the input file is opened"; //A safety check
+    cout << firstLineOfFile << "\n"; //prints to terminal to verify
+
+    fin.close();
+
+
+
+
+    //Input Output Test Code 
+      //cout << pow(2, 10);
+
+    string westWind = "Zephyrus";
+
+    string northWind = "Boreas";
+    //cout << westWind.length();
+
+    //cout << sqrt("sixty four");
+    //cout << sqrt(64);
+
+    string dog = "perro";
+
+    string cat = "gato";
+
+    //cout << length(dog);
+
+    char ch;
+    cin.get(ch);
+    //cout << "Here: " << ch;
 }
---
+++ September15/September15.cpp
@@ -0,0 +1,136 @@
+#include<cmath> // This is a library that allows you to use math functions like pow(), sin(), cos(), etc.
+#include<iostream> 
+#include<string>
+#include<algorithm>
+#include<iomanip>
+
+using namespace std;
+// We are going to write to an Output file here. Saving our results to a file.
+#include<fstream> // f stands for file.
+#include<iostream> // io stands for input/output.
+
+using namespace std;
+
+int main()
+{
+    unsigned int a = -123; 
+    cout << a << "\n";
+
+
+    ofstream fout("SomeText.txt"); //output file fout(filename) //the .txt is a file extension.
+
+    //Loop memory error
+    //while (true)
+    //{
+    //    fout << "a\n";
+    //}
+
+    fout << "asdasdf\n";
+    fout << "This is the second line of output written to the file\n";
+
+    fout.close();
+
+    ofstream outputData;
+
+    outputData.open("outputFile.txt");
+
+    outputData << "Hello world" << endl;
+
+    fout.close();
+    //The output is found through open in file explorer and open the text file in the project.
+    //close the file when done writing to it. This is important because if you don't close the file, the data may not be saved properly.
+        // file extensions are used by an OS to determine the app used to open the file
+        //ex: .jpg files open with windows photo viewer, .mp3 files open with windows media player, .txt files open with notepad, etc.
+        // .txt files open with notepad
+
+    //read input from a already existing file:
+    ifstream fin("SomeText.txt");
+
+    if (fin.is_open() == false) //if the file was not found or opened
+    {
+        cout << "file was not found\n";
+        return -1; // early return
+    }
+
+    string firstLineOfFile; 
+    string secondLineOfFile;
+    getline(fin, firstLineOfFile); //Similar to how we used it before but translated from getline(cin, variable);
+    getline(fin, secondLineOfFile);
+    cout << "Does this line get printed if the input file is opened\n"; //A safety check
+    cout << firstLineOfFile << "\n"; //prints to terminal to verify
+    cout << secondLineOfFile << "\n"; //Works properly
+
+    fin.close();
+
+
+
+
+//Input Output Test Code 
+  //cout << pow(2, 10);
+
+  string westWind = "Zephyrus";
+
+  string northWind = "Boreas";
+  //cout << westWind.length();
+
+  //cout << sqrt("sixty four");
+  //cout << sqrt(64);
+
+  string dog = "perro";
+
+  string cat = "gato";
+
+  //cout << length(dog);
+
+  //cin.ignore();
+  char ch;
+  //cin.get(ch);
+  //cout << "Here: " << ch;
+
+  //string username;
+  //cout << "Enter your name: " << endl;
+  //cin.ignore(3);
+  //cin >> username;
+  //cout << username;
+
+  const double eulerNumber = 2.71828;
+  //cout << fixed;
+  //cout << setprecision(2);
+  //cout << eulerNumber;
+
+  double minimumEarthTiltAngle = 22.1; // degrees
+
+  double currentTropicOfCancerLattitude = 23.44; //also degrees
+  //cout << fixed;
+  //cout << setprecision(1);
+  //cout << currentTropicOfCancerLattitude - minimumEarthTiltAngle << endl;
+
+//  int number;
+//  cout << "Enter a number: " << endl;
+//  cin >> number;
+//  cout << "You entered: " << number << endl;
+////needs a cin.ignore();
+//  cin.ignore();
+//  string message;
+//  cout << "Enter a message: " << endl;
+//  getline(cin, message);
+//  cout << "You entered: " << message << endl;
+//
+//  string stringName = "camelCase";
+//  string StringName = "PascalCase";
+
+  int leg1, leg2, hypotenuse;
+  leg1 = 3;
+  leg2 = 4;
+
+  //sqrt(leg1 * leg1 + leg2 * leg2) = hypotenuse;
+  //hypotenuse = (leg1 * leg1 + leg2 * leg2);
+  //cout << "first" << hypotenuse << endl;
+  //hypotenuse = sqrt(hypotenuse);
+  //cout << "second" << hypotenuse << endl; //corrected code to properly sqaure root
+
+  string palindrome = "step on no pets";
+
+  cout << palindrome.length() << endl;
+
+}
